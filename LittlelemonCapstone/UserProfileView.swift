bdf2d355001775b27dd20565040f319db7632eed
satar <abdisatar.mohamed@pixeledge.io>
2024-05-05 14:43:59 +0300

    feat: add actions for discard and save changes buttons

--- LittlelemonCapstone/UserProfileView.swift
+++ LittlelemonCapstone/UserProfileView.swift
@@ -17,154 +17,203 @@ struct UserProfileView: View {
     @State private var firstName: String = ""
     @State private var lastName: String = ""
     @State private var email: String = ""
-    @State private var doesClose = false
+    @State private var orderStatuses = false
+    @State private var passwordChanges = false
+    @State private var specialOffers = false
+    @State private var newsLetter = false
+    
 
     var body: some View {
         NavigationView{
             
             
-            VStack(alignment:.leading){
-                
-                HStack {
-                   
-                    Image(systemName: "arrowshape.left.circle.fill")
-                        .font(.system(size: 26))
-                        .padding(.leading)
-                        .foregroundColor(.primaryColor1)
-
-                    Spacer()
-                        Image("Logo")  .padding([.trailing,.leading])
-                        Spacer()
-                        
-                        Image("profile-image-placeholder")
-                            .resizable()
-                            .aspectRatio( contentMode: .fit)
-                        
-                            .clipShape(Circle())
-                            .frame(maxHeight: 50)
-                            .padding([.trailing])
-                        
-
-                }
-               
-                HStack {
-                    Text("Personal information")
-                        .font(.title2)
-                        .multilineTextAlignment(.leading)
-//                        .frame(maxWidth: .infinity)
-                        .fontWeight(.bold)
-                        .padding(.leading,18)
-                        
-                    Spacer()
-                }
-                
-                
-                VStack(alignment: .leading) {
-                    Text("First Name")
-                        .padding(.leading,20)
-                        .padding(.top,1)
-                        .padding(.bottom,-10)
-                        .foregroundColor(.gray)
+                VStack(alignment:.leading){
                     
-                    TextField(
-                        "First name ",
-                        text: $firstName
-                    )
-                    .textInputAutocapitalization(.never)
-                    .disableAutocorrection(true)
-                    .textFieldStyle(MyTextFieldStyle())
-                }
-               
-                VStack(alignment: .leading) {
-                    Text("Last Name").padding(.leading,20)
-                        .padding(.top,1)
-                        .padding(.bottom,-10)
-                        .foregroundColor(.gray)
-                    TextField(
-                        "Last Name",
-                        text: $lastName
-                    ).textFieldStyle(MyTextFieldStyle())
+                    HStack {
+                       
+                        Image(systemName: "arrowshape.left.circle.fill")
+                            .font(.system(size: 26))
+                            .padding(.leading)
+                            .foregroundColor(.primaryColor1)
 
-                    .textInputAutocapitalization(.never)
-                    .disableAutocorrection(true)
-                }
+                        Spacer()
+                            Image("Logo")  .padding([.trailing,.leading])
+                            Spacer()
+                            
+                            Image("profile-image-placeholder")
+                                .resizable()
+                                .aspectRatio( contentMode: .fit)
+                            
+                                .clipShape(Circle())
+                                .frame(maxHeight: 50)
+                                .padding([.trailing])
+                            
 
-               
-                VStack(alignment: .leading) {
-                    Text("Last Name")
-                        .padding(.leading,20)
-                        .padding(.top,1)
-                        .padding(.bottom,-10)
-                        .foregroundColor(.gray)
-                    TextField(
-                        "Email",
-                        text: $email
-                    ).textFieldStyle(MyTextFieldStyle())
-
-                    .textInputAutocapitalization(.never)
-                    .disableAutocorrection(true)
-                }
-                
-                HStack {
-                    Text("Email notifications")
-                        .font(.title3)
-                        .multilineTextAlignment(.leading)
-//                        .frame(maxWidth: .infinity)
-                        .fontWeight(.bold)
-                        .padding(.leading,18)
-                        
-                    Spacer()
-                }.padding([.bottom,.top])
-                
-                HStack() {
-                    Toggle("", isOn: $doesClose)
-                        .toggleStyle(CheckboxToggleStyle()).padding(.leading,20)
-                    Text("Order Statuses")
-                    Spacer()
-                }.padding(.bottom)
-               
-                HStack() {
-                    Toggle("", isOn: $doesClose)
-                        .toggleStyle(CheckboxToggleStyle()).padding(.leading,20)
-                    Text("Password Changes")
-                    Spacer()
-                }.padding(.bottom)
-                
-                HStack() {
-                    Toggle("", isOn: $doesClose)
-                        .toggleStyle(CheckboxToggleStyle()).padding(.leading,20)
-                    Text("Special Offers")
-                    Spacer()
-                }.padding(.bottom)
-                
-                HStack() {
-                    Toggle("", isOn: $doesClose)
-                        .toggleStyle(CheckboxToggleStyle()).padding(.leading,20)
-                    Text("Email Notificatitons")
-                    Spacer()
-                }.padding(.bottom)
-            
-                
-                
-                Button("Logout"){
-                    UserDefaults.standard.set(false, forKey: kIsLoggedIn)
-                    self.presentation.wrappedValue.dismiss()
-                }.foregroundColor(.primaryColor1)
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.secondaryColor1,in: RoundedRectangle(
-                        cornerRadius: 8,
-                        style: .continuous
-                    )) .padding()
+                    }
+                    ScrollView{
                    
-                Spacer()
-            }.onAppear(){
+                    HStack {
+                        Text("Personal information")
+                            .font(.title2)
+                            .multilineTextAlignment(.leading)
+   
+                            .fontWeight(.bold)
+                            .padding(.leading,18)
+                            
+                        Spacer()
+                    }
+                    
+                    
+                    VStack(alignment: .leading) {
+                        Text("First Name")
+                            .padding(.leading,20)
+                            .padding(.top,1)
+                            .padding(.bottom,-10)
+                            .foregroundColor(.gray)
+                        
+                        TextField(
+                            "First name ",
+                            text: $firstName
+                        )
+                        .textInputAutocapitalization(.never)
+                        .disableAutocorrection(true)
+                        .textFieldStyle(MyTextFieldStyle())
+                    }
+                   
+                    VStack(alignment: .leading) {
+                        Text("Last Name").padding(.leading,20)
+                            .padding(.top,1)
+                            .padding(.bottom,-10)
+                            .foregroundColor(.gray)
+                        TextField(
+                            "Last Name",
+                            text: $lastName
+                        ).textFieldStyle(MyTextFieldStyle())
+
+                        .textInputAutocapitalization(.never)
+                        .disableAutocorrection(true)
+                    }
+
+                   
+                    VStack(alignment: .leading) {
+                        Text("Last Name")
+                            .padding(.leading,20)
+                            .padding(.top,1)
+                            .padding(.bottom,-10)
+                            .foregroundColor(.gray)
+                        TextField(
+                            "Email",
+                            text: $email
+                        ).textFieldStyle(MyTextFieldStyle())
+
+                        .textInputAutocapitalization(.never)
+                        .disableAutocorrection(true)
+                    }
+                    
+                    HStack {
+                        Text("Email notifications")
+                            .font(.title3)
+                            .multilineTextAlignment(.leading)
+    //                        .frame(maxWidth: .infinity)
+                            .fontWeight(.bold)
+                            .padding(.leading,18)
+                            
+                        Spacer()
+                    }.padding([.bottom,.top])
+                    
+                    HStack() {
+                        Toggle("", isOn: $orderStatuses)
+                            .toggleStyle(CheckboxToggleStyle()).padding(.leading,20)
+                        Text("Order Statuses")
+                        Spacer()
+                    }.padding(.bottom)
+                   
+                    HStack() {
+                        Toggle("", isOn: $passwordChanges)
+                            .toggleStyle(CheckboxToggleStyle()).padding(.leading,20)
+                        Text("Password Changes")
+                        Spacer()
+                    }.padding(.bottom)
+                    
+                    HStack() {
+                        Toggle("", isOn: $specialOffers)
+                            .toggleStyle(CheckboxToggleStyle()).padding(.leading,20)
+                        Text("Special Offers")
+                        Spacer()
+                    }.padding(.bottom)
+                    
+                    HStack() {
+                        Toggle("", isOn: $newsLetter)
+                            .toggleStyle(CheckboxToggleStyle()).padding(.leading,20)
+                        Text("Newsletter")
+                        Spacer()
+                    }.padding(.bottom)
                 
-                firstName = UserDefaults.standard.string(forKey: kFirstName) ?? ""
-                lastName = UserDefaults.standard.string(forKey: kLastName) ?? ""
-                email = UserDefaults.standard.string(forKey: kEmail) ?? ""
+                    
+                    
+                    Button("Logout"){
+                        UserDefaults.standard.set(false, forKey: kIsLoggedIn)
+                        self.presentation.wrappedValue.dismiss()
+                    }.foregroundColor(.primaryColor1)
+                        .padding()
+                        .fontWeight(Font.Weight.bold)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.secondaryColor1,in: RoundedRectangle(
+                            cornerRadius: 8,
+                            style: .continuous
+                        )) .padding()
+                       
+                    Spacer()
+                    
+                    HStack{
+                        
+                        Button("Discard changes"){
+                            
+                            firstName = UserDefaults.standard.string(forKey: kFirstName) ?? ""
+                            lastName = UserDefaults.standard.string(forKey: kLastName) ?? ""
+                            email = UserDefaults.standard.string(forKey: kEmail) ?? ""
+                        }.foregroundColor(.primaryColor1)
+                            .fontWeight(Font.Weight.bold)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .border(.black)
+                            .cornerRadius(3.5)
+                             .padding(.leading)
+                        
+                        Button("Save changes"){
+                          
+                            
+                            
+                            if !firstName.isEmpty && !lastName.isEmpty && !email.isEmpty {
+                                // Store user data in UserDefaults
+                                UserDefaults.standard.set(firstName, forKey: kFirstName)
+                                UserDefaults.standard.set(lastName, forKey: kLastName)
+                                UserDefaults.standard.set(email, forKey: kEmail)
+                                UserDefaults.standard.set(true, forKey: kIsLoggedIn)
+                              
+                               
+                            }
+                            
+                            
+                        }.foregroundColor(.white)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.primaryColor1,in: RoundedRectangle(
+                                cornerRadius: 8,
+                                style: .continuous
+                            )) .padding(.trailing)
+                    }
+                    
+                }
+                .onAppear(){
+                    
+                    firstName = UserDefaults.standard.string(forKey: kFirstName) ?? ""
+                    lastName = UserDefaults.standard.string(forKey: kLastName) ?? ""
+                    email = UserDefaults.standard.string(forKey: kEmail) ?? ""
+                }
             }
-            .navigationBarTitle("", displayMode: .inline)
+         
+//            .navigationBarTitle("", displayMode: .inline)
         }.navigationBarBackButtonHidden(true)
       
     }
